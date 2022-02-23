510610d29d775e20c4497e187a0cc537116c4624
Bao Loc Pham <67360122+BaoLocPham@users.noreply.github.com>
2022-02-23 11:26:41 +0700

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,2 +1,2 @@
-python /utils/preprocess_test.py --test_type public_test
+python ./utils/preprocess_test.py --test_type public_test
 python search.py --data /preprocessed/public_test --output /result/submission.csv
