c924b482ad566387298b04a0138eae0bda9ef50d
Bao Loc Pham <67360122+BaoLocPham@users.noreply.github.com>
2022-03-09 18:11:54 +0700

    update predict_private_test.sh

--- predict_private_test.sh
+++ predict_private_test.sh
@@ -1,2 +1,2 @@
-python /model/utils/preprocess_test.py --test_type private_test
-python /model/search.py --data /preprocessed/private_test --output /result/submission.csv
+python ./utils/preprocess_test.py --test_type private_test
+python search.py --data /content/drive/MyDrive/ZaloAI_Hum_To_Song/preprocessed/private_test/ --output /content/result/private_test_submission.csv
